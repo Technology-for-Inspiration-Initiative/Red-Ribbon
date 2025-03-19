570d8743dcb78c62021f20cce3768532e5ce8a0b
Emmanuela Adanna Ononiwu <writetousinspireit@gmail.com>
2025-03-19 18:27:51 +0100

    Updated files

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('#actionButton');
-    if (button) {
-      button.addEventListener('click', () => {
-        alert('Thank you for visiting The Red Ribbon Platform!');
-      });
-    }
-  });
-  
